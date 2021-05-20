2244f5b71f4e173e6875d48366694901859b635a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-05-20 01:09:21 +0100

    Add beans/graphics/relational.py

---
+++ beans/graphics/relational.py
@@ -0,0 +1,44 @@
+"""
+Module relational
+"""
+import matplotlib.pyplot as plt
+
+
+class Relational:
+    """
+    Class Relational
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+    @staticmethod
+    def figure(width: float, height: float):
+        """
+
+        :param width: The width of the graph
+        :param height: The height of the graph
+        :return:
+        """
+
+        fig = plt.figure(figsize=(width, height))
+        handle = fig.gca()
+        handle.tick_params(axis='both', labelsize='small')
+
+        return handle
+
+    @staticmethod
+    def annotation(handle, labels):
+        """
+
+        :param handle: The handle of the graph that will be annotated
+        :param labels: collections.namedtuple(
+                            typename='RelationalGraphLabels',
+                            field_names=['title', 'xlabel', 'ylabel'])
+        :return:
+        """
+        handle.set_title(labels.title, fontsize='small')
+        handle.set_xlabel(labels.xlabel, fontsize='small')
+        handle.set_ylabel(labels.ylabel, fontsize='small')
