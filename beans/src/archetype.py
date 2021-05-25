3d894a37cb6238efceb5a9390d04bbe9f7864db9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-05-25 17:27:28 +0100

    Update beans/src/archetype.py

--- beans/src/archetype.py
+++ beans/src/archetype.py
@@ -11,8 +11,8 @@ class Archetype:
         The  original zip file
             http://archive.ics.uci.edu/ml/machine-learning-databases/00602/DryBeanDataset.zip
 
-        The develop's copy of this original zip file
-            https://github.com/exhypotheses/beans/raw/develop/data/beans.zip
+        The developer's copy of this original zip file
+            https://github.com/miscellane/hub/raw/develop/data/beans/beans.zip
         """
 
     @staticmethod
@@ -24,8 +24,8 @@ class Archetype:
         InstancesAttributes = collections.namedtuple(
             typename='InstancesAttributes', field_names=['url', 'labels'])
 
-        # A copy of this original zip file
-        url = 'https://raw.githubusercontent.com/exhypotheses/beans/develop/data/beans.csv'
+        # The CSV version of the beans data
+        url = 'https://raw.githubusercontent.com/miscellane/hub/develop/data/beans/beans.csv'
 
         # The field that hosts the labels/classes
         labels = 'Class'
