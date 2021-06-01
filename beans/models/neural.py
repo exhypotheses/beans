f096ce8da825076ba6268a284a87d760e4eab14b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-01 09:37:41 +0100

    Update beans/models/neural.py

--- beans/models/neural.py
+++ beans/models/neural.py
@@ -57,16 +57,15 @@ class Neural:
             ann_output = pymc3.Data('ann_output', output)
 
             # Weights from input to hidden layer
-            weights_in_1 = pymc3.Normal(name='w_in_1', mu=0, sigma=1.5, shape=(features.shape[1], hidden_1),
-                                        testval=init_1)
+            weights_in_1 = pymc3.StudentT(name='w_in_1', nu=1, mu=0, sigma=1.5, shape=(features.shape[1], hidden_1),
+                                          testval=init_1)
 
             # Weights from 1st to 2nd layer
-            weights_1_2 = pymc3.Normal(name='w_1_2', mu=0, sigma=1.5, shape=(hidden_1, hidden_2),
-                                       testval=init_2)
+            weights_1_2 = pymc3.StudentT(name='w_1_2', nu=1, mu=0, sigma=1.5, shape=(hidden_1, hidden_2), testval=init_2)
 
             # Weights from hidden layer to output
-            weights_2_out = pymc3.Normal(name='w_2_out', mu=0, sigma=1.5, shape=(hidden_2, output.shape[1]),
-                                         testval=init_out)
+            weights_2_out = pymc3.StudentT(name='w_2_out', nu=1, mu=0, sigma=1.5, shape=(hidden_2, output.shape[1]),
+                                           testval=init_out)
 
             # Build neural-network using tanh activation function
             act_1 = pymc3.math.tanh(pymc3.math.dot(ann_input, weights_in_1))
