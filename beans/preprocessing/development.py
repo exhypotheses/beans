c41d952aa2a9b81272a16ec46dc0374a941b1b5d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-04 11:14:36 +0100

    Update beans/preprocessing/development.py

--- beans/preprocessing/development.py
+++ beans/preprocessing/development.py
@@ -7,24 +7,26 @@ import theano
 import beans.functions.sample
 import beans.functions.scale
 import beans.functions.knee
+import beans.functions.project
 import beans.functions.encode
 
 
 class Development:
 
-    def __init__(self, training_split: pd.DataFrame, labels: list, target: str):
+    def __init__(self, data: pd.DataFrame, labels: list, target: str):
         """
 
-        :param training_split: The training data
+        :param data: The training data
         :param labels: The list of distinct labels in the target field
         :param target: The target field
         """
 
-        self.training_split = training_split
+        self.data = data
         self.labels = labels
         self.target = target
 
-        logging.basicConfig(level=logging.WARNING, format='%(message)s\n%(asctime)s.%(msecs)03d', datefmt='%Y-%m-%d %H:%M:%S')
+        logging.basicConfig(level=logging.WARNING, format='%(message)s\n%(asctime)s.%(msecs)03d', 
+                            datefmt='%Y-%m-%d %H:%M:%S')
         self.logger = logging.getLogger(__name__)
 
     def sample_(self):
@@ -36,28 +38,49 @@ class Development:
 
         sample = beans.functions.sample.Sample()
 
-        return sample.exc(blob=self.training_split, target=self.target)
+        return sample.exc(blob=self.data, target=self.target)
 
-    def scale_(self, training_sampled: pd.DataFrame):
+    def scale_(self, blob: pd.DataFrame):
         """
-        Scales the sampled data
+        Scales the independent variables of blob
 
-        :param training_sampled:  The sampled data
+        :param blob:  The data
+        :return:
+        """
+        
+        focus = blob.copy().drop(columns=self.target)
+
+        scale = beans.functions.scale.Scale()
+        scaler = scale.exc(blob=focus)
+        x_scaled = scale.apply(blob=focus, scaler=scaler)
+        scaled = pd.concat((x_scaled, blob[self.target]), axis=1, ignore_index=False)
+
+        return scaled, scaler
+
+    def project_(self, blob: pd.DataFrame):
+        """
+
+        :param blob:  The data that will be projected
         :return:
         """
 
-        scale = beans.functions.scale.Scale()
-        scaler = scale.exc(blob=training_sampled.drop(columns=self.target))
-        x_training_scaled = scale.apply(blob=training_sampled.drop(columns=self.target), scaler=scaler)
-        training_scaled = pd.concat((x_training_scaled, training_sampled[self.target]), axis=1, ignore_index=False)
+        knee = beans.functions.knee.Knee()
+        n_components = knee.exc(blob=blob, target=self.target)
+        self.logger.warning('The # of Kernel Principal Components will be set to {}'.format(n_components))
 
-        return training_scaled, scaler
+        project = beans.functions.project.Project()
+        matrix = blob.drop(columns=self.target).to_numpy()
+        projector = project.exc(matrix=matrix, n_components=n_components)
+        projected = project.apply(matrix=matrix, vector=blob[self.target], projector=projector)
 
-    def encode_(self, training_scaled: pd.DataFrame):
+        return projected, projector
+
+    def encode_(self, blob: pd.DataFrame):
         """
-        One-hot-encodes the depedent variable
+        One-hot-encodes the dependent variable
 
-        :param training_scaled: In general, this will be the DataFrame w.r.t. the final preprocessing step before structuring
+        :param blob: In general, this will be the DataFrame w.r.t. the final preprocessing step 
+                                              before structuring
         :return:
         """
 
@@ -69,45 +92,24 @@ class Development:
         categories = CategoricalData._make((fields, arrays))
 
         encode = beans.functions.encode.Encode()
-        bits = encode.bits(frame=training_scaled, categories=categories)
-        training_encoded = pd.concat((training_scaled.drop(columns=fields), bits), axis=1, ignore_index=False)
+        bits = encode.bits(frame=blob, categories=categories)
+        encoded = pd.concat((blob.drop(columns=fields), bits), axis=1, ignore_index=False)
 
-        return training_encoded
+        return encoded
 
-    def structure(self, training_encoded: pd.DataFrame):
+    def structure(self, blob: pd.DataFrame):
         """
         Structures the training data according to what is expected by the neural network model
 
-        :param training_encoded:
+        :param blob:
         :return:
         """
 
-        x_training_encoded = training_encoded.drop(columns=self.labels).to_numpy()
-        y_training_encoded = training_encoded[self.labels].to_numpy()
+        x_matrix = blob.drop(columns=self.labels).to_numpy()
+        y_matrix = blob[self.labels].to_numpy()
 
-        unity = np.ones((x_training_encoded.shape[0], 1))
-        x_training_points = np.concatenate((unity, x_training_encoded), axis=1).astype(theano.config.floatX)
-        y_training_points = y_training_encoded.astype(theano.config.floatX)
+        unity = np.ones((x_matrix.shape[0], 1))
+        x_points = np.concatenate((unity, x_matrix), axis=1).astype(theano.config.floatX)
+        y_points = y_matrix.astype(theano.config.floatX)
 
-        return x_training_points, y_training_points
-
-    def exc(self):
-        """
-
-        :return:
-        """
-
-        training_sampled = self.sample_()
-        self.logger.warning('\n1. The training data shape after SVNSMOTE sampling: {}'.format(training_sampled.shape))
-        self.logger.warning('\nFrequencies:\n{}'.format(training_sampled['class'].value_counts()))
-
-        training_scaled, scaler = self.scale_(training_sampled=training_sampled)
-        self.logger.warning('\n2. The training data shape after scaling: {}'.format(training_scaled.shape))
-
-        training_encoded = self.encode_(training_scaled=training_scaled)
-        self.logger.warning('\n3. The training data shape after one-hot-encoding the dependent variable: {}'.format(training_encoded.shape))
-
-        x_training_points, y_training_points = self.structure(training_encoded=training_encoded)
-        self.logger.warning('\n4. The training matrices after structuring: x -> {}, y -> {}'.format(x_training_points.shape, y_training_points.shape))
-
-        return x_training_points, y_training_points, scaler
+        return x_points, y_points
