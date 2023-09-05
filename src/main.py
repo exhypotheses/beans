73ee1b555f0a9f2ba04d162c172d3c50133cce4a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-05 07:24:37 +0100

    Update src/main.py

---
+++ src/main.py
@@ -0,0 +1,40 @@
+"""
+The main module for running other classes
+"""
+import logging
+import os
+import sys
+import time
+
+import pymc
+import jax
+
+
+def main():
+    """
+    Entry point
+    :return: None
+    """
+
+    # Notes
+    logger.info('JAX')
+    logger.info(jax.devices(backend='gpu'))
+    logger.info(f"The number of GPU devices: {jax.device_count(backend='gpu')}")
+
+
+if __name__ == '__main__':
+
+    # Paths
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    main()
