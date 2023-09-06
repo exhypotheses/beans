301313218427f3a2dcef929d94a41e853cd3db24
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-06 14:46:19 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 """The global definitions"""
 import os
+import collections
 import numpy as np
 
 
@@ -7,6 +8,8 @@ class Config:
     """
     Config
     """
+
+    Meta = collections.namedtuple(typename='Meta', field_names=['uri', 'dtype', 'rename', 'dependent', 'numeric'])
 
     def __init__(self):
         """
@@ -22,11 +25,16 @@ class Config:
         # Training fraction
         self.train_size = 0.735
 
+        # beans
+        self.meta = self.__get_meta()
+
+    def __get_meta(self) -> Meta:
+
         # The beans data
-        self.uri: str = os.path.join(os.getcwd(), 'data', 'beans.csv')
+        uri: str = os.path.join(os.getcwd(), 'data', 'beans.csv')
 
         # The original field names, and their data types
-        self.dtype = {
+        dtype = {
             'Area': int, 'Perimeter': np.float64, 'MajorAxisLength': np.float64, 
             'MinorAxisLength': np.float64, 'AspectRation': np.float64, 'Eccentricity': np.float64, 
             'ConvexArea': int, 'EquivDiameter': np.float64, 'Extent': np.float64, 'Solidity': np.float64,
@@ -34,7 +42,7 @@ class Config:
             'ShapeFactor2': np.float64, 'ShapeFactor3': np.float64, 'ShapeFactor4': np.float64, 'Class': str}
 
         # Renaming ...
-        self.rename: dict[str, str] = {
+        rename: dict[str, str] = {
             'Area': 'area', 'Perimeter': 'perimeter', 'MajorAxisLength': 'major_axis_length', 
             'MinorAxisLength': 'minor_axis_length', 'AspectRation': 'aspect_ratio', 'Eccentricity': 'eccentricity', 
             'ConvexArea': 'convex_area', 'EquivDiameter': 'equiv_diameter', 'Extent': 'extent', 'Solidity': 'solidity',
@@ -43,9 +51,11 @@ class Config:
             'Class': 'class'}
 
         # The name of the dependent variable field
-        self.dependent: str = 'class'
+        dependent: str = 'class'
 
         # The numeric fields
-        self.numeric = ['area', 'perimeter', 'major_axis_length', 'minor_axis_length', 'aspect_ratio', 'eccentricity',
+        numeric = ['area', 'perimeter', 'major_axis_length', 'minor_axis_length', 'aspect_ratio', 'eccentricity',
                          'convex_area', 'equiv_diameter', 'extent', 'solidity', 'roundness', 'compactness', 'shape_factor_1', 
                          'shape_factor_2', 'shape_factor_3', 'shape_factor_4']
+
+        return self.Meta(uri=uri, dtype=dtype, rename=rename, dependent=dependent, numeric=numeric)
