5c93883c6f7b26338a089425f18afd2eb9406e04
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-06 14:46:33 +0100

    Update src/algorithms/split.py

--- src/algorithms/split.py
+++ src/algorithms/split.py
@@ -17,6 +17,7 @@ class Split:
         """
 
         self.__configurations = config.Config()
+        self.__meta = self.__configurations.meta
 
     def exc(self, data: pd.DataFrame, train_size: float) -> (pd.DataFrame, pd.DataFrame):
         """
@@ -27,11 +28,11 @@ class Split:
         """
 
         x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-            data.drop(columns=self.__configurations.dependent),
-            data[self.__configurations.dependent],
+            data.drop(columns=self.__meta.dependent),
+            data[self.__meta.dependent],
             train_size=train_size,
             random_state=self.__configurations.seed,
-            stratify=data[self.__configurations.dependent])
+            stratify=data[self.__meta.dependent])
 
         training = pd.concat((x_train.reset_index(drop=True), y_train.reset_index(drop=True)), axis=1,
                              ignore_index=False)
