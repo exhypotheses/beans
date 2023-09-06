f1087eeb42c3d5201667e2abfa470e4b23efe3e2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-06 07:59:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 """The global definitions"""
 import os
+import numpy as np
 
 
 class Config:
@@ -17,6 +18,23 @@ class Config:
 
         # The seed number for all algorithms
         self.seed: int = 5
+
+        # The original field names, and their data types
+        self.dtype = {
+            'Area': int, 'Perimeter': np.float64, 'MajorAxisLength': np.float64, 
+            'MinorAxisLength': np.float64, 'AspectRation': np.float64, 'Eccentricity': np.float64, 
+            'ConvexArea': int, 'EquivDiameter': np.float64, 'Extent': np.float64, 'Solidity': np.float64,
+            'roundness': np.float64, 'Compactness': np.float64, 'ShapeFactor1': np.float64, 
+            'ShapeFactor2': np.float64, 'ShapeFactor3': np.float64, 'ShapeFactor4': np.float64, 'Class': str}
+
+        # Renaming ...
+        self.rename = {
+            'Area': 'area', 'Perimeter': 'perimeter', 'MajorAxisLength': 'major_axis_length', 
+            'MinorAxisLength': 'minor_axis_length', 'AspectRation': 'aspect_ratio', 'Eccentricity': 'eccentricity', 
+            'ConvexArea': 'convex_area', 'EquivDiameter': 'equiv_diameter', 'Extent': 'extent', 'Solidity': 'solidity',
+            'roundness': 'roundness', 'Compactness': 'compactness', 'ShapeFactor1': 'shape_factor_1', 
+            'ShapeFactor2': 'shape_factor_2', 'ShapeFactor3': 'shape_factor_3', 'ShapeFactor4': 'shape_factor_4', 
+            'Class': 'class'}
 
         # The name of the dependent variable field
         self.dependent: str = 'class'
