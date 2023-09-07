59f04695af08722da01c7233b165a28f01b61186
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-07 15:09:13 +0100

    Update src/algorithms/project.py

--- src/algorithms/project.py
+++ src/algorithms/project.py
@@ -19,14 +19,13 @@ class Project:
 
         configurations = config.Config()
         self.__seed = configurations.seed
-        self.__meta = configurations.meta
 
     @staticmethod
-    def __project(matrix: np.ndarray, vector: pd.Series, projector: skd.KernelPCA) -> pd.DataFrame:
+    def __project(matrix: np.ndarray, outcomes: pd.DataFrame, projector: skd.KernelPCA) -> pd.DataFrame:
         """
 
         :param matrix: The independent variables matrix
-        :param vector: The dependent variable vector
+        :param outcomes: The data frame of the outcome/s fields
         :param projector: A transformer
         :return:
         """
@@ -34,7 +33,7 @@ class Project:
         principals = projector.transform(X=matrix)
         names = ['kpc_' + str(i).zfill(2) for i in range(1, 1 + principals.shape[1])]
 
-        return pd.concat((pd.DataFrame(data=principals, columns=names), vector), axis=1)
+        return pd.concat((pd.DataFrame(data=principals, columns=names), outcomes), axis=1, ignore_index=True)
 
     def __projector(self, matrix: np.ndarray,  n_components: int = None) -> skd.KernelPCA:
         """
@@ -48,26 +47,30 @@ class Project:
 
         return projector
 
-    def exc(self, blob: pd.DataFrame,  n_components: int = None, projector: skd.KernelPCA = None) -> (
+    def exc(self, blob: pd.DataFrame, exclude: list = None,  n_components: int = None, projector: skd.KernelPCA = None) -> (
             pd.DataFrame, skd.KernelPCA):
         """
 
-        :param blob: 
+        :param blob: The data
+        :param exclude: The list of fields to exclude from the projection step
         :param n_components: The number of components of interest
         :return:
         """
 
         # The matrix that will be decomposed
-        matrix: np.ndarray = blob.copy().drop(columns=self.__meta.dependent).array
+        matrix: np.ndarray = blob.copy().drop(columns=exclude).array
 
         # The parallel dependent variable vector, as a series
-        vector: pd.Series = blob.copy()[self.__meta.dependent]
+        if exclude is None:
+            outcomes = pd.DataFrame()
+        else:
+            outcomes: pd.DataFrame = blob.copy()[exclude]
 
         # The projector
         if projector is None:
             projector = self.__projector(matrix=matrix, n_components=n_components)
 
         # Projecting
-        projected: pd.DataFrame = self.__project(matrix=matrix, vector=vector, projector=projector)
+        projected: pd.DataFrame = self.__project(matrix=matrix, outcomes=outcomes, projector=projector)
 
         return projected, projector
