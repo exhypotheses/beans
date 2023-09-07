f3cade2b7d75b6164c54854d005d721f92153e2e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-07 13:36:20 +0100

    Update src/algorithms/knee.py

--- src/algorithms/knee.py
+++ src/algorithms/knee.py
@@ -1,38 +1,41 @@
+"""Principal vectors knee"""
 import pandas as pd
 import sklearn.cluster
-import yellowbrick.cluster
-
-import beans.graphing.relational
+import yellowbrick
 
 import config
+import src.graphing.relational
 
 
 class Knee:
+    """
+    Knee
+        Determines the kneepoint 
+    """
 
     def __init__(self):
         """
         Constructor
         """
 
-        configurations = config.Config()
-        self.SEED = configurations.SEED
+        self.__seed = config.Config().seed
+        self.__meta = config.Config().meta
 
-        self.relational = beans.graphing.relational.Relational()
+        self.relational = src.graphing.relational.Relational()
 
-    def exc(self, blob: pd.DataFrame, target: str) -> int:
+    def exc(self, blob: pd.DataFrame) -> int:
         """
 
         :param blob: The data set that will be sampled
-        :param target: The labels field
         :return:
         """
 
-        ax = self.relational.figure(width=3.9, height=3.3)
+        axes = self.relational.figure(width=3.9, height=3.3)
 
         # A rough estimate of the number of effective clusters that <blob> can be divided into
-        kmc = sklearn.cluster.KMeans(random_state=self.SEED, max_iter=1000, algorithm='full')
-        ybc = yellowbrick.cluster.KElbowVisualizer(estimator=kmc, k=(3, 16), metric='distortion', timings=False, ax=ax)
-        ybc.fit(X=blob.drop(columns=target))
-        ax.figure.clf()
+        kmc = sklearn.cluster.KMeans(random_state=self.__seed, max_iter=1000, algorithm='full')
+        ybc = yellowbrick.cluster.KElbowVisualizer(estimator=kmc, k=(3, 16), metric='distortion', timings=False, ax=axes)
+        ybc.fit(X=blob.drop(columns=self.__meta.dependent))
+        axes.figure.clf()
 
         return ybc.elbow_value_
