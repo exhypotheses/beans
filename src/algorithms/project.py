b406237689e11e3bd9d0670e21e0eff3301c2f37
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-07 14:42:39 +0100

    Update src/algorithms/project.py

--- src/algorithms/project.py
+++ src/algorithms/project.py
@@ -1,12 +1,16 @@
+"""Projecting"""
 import numpy as np
 import pandas as pd
 
-import sklearn.decomposition
+import sklearn.decomposition as skd
 
 import config
 
 
 class Project:
+    """
+    Project
+    """
 
     def __init__(self):
         """
@@ -14,14 +18,15 @@ class Project:
         """
 
         configurations = config.Config()
-        self.SEED = configurations.SEED
+        self.__seed = configurations.seed
+        self.__meta = configurations.meta
 
     @staticmethod
-    def apply(matrix: np.ndarray, vector: pd.Series, projector: sklearn.decomposition.KernelPCA) -> pd.DataFrame:
+    def __project(matrix: np.ndarray, vector: pd.Series, projector: skd.KernelPCA) -> pd.DataFrame:
         """
 
         :param matrix: The independent variables matrix
-        :param vector:  The dependent variable vector
+        :param vector: The dependent variable vector
         :param projector: A transformer
         :return:
         """
@@ -31,15 +36,38 @@ class Project:
 
         return pd.concat((pd.DataFrame(data=principals, columns=names), vector), axis=1)
 
-    def exc(self, matrix: np.ndarray,  n_components: int = None) -> sklearn.decomposition.KernelPCA:
+    def __projector(self, matrix: np.ndarray,  n_components: int = None) -> skd.KernelPCA:
+        """
+        
+        :param matrix: The independent variables matrix
+        :param n_components: The number of components of interest
         """
 
-        :param matrix: The matrix that will be decomposed
-        :param n_components: The number of components required
-        :return:
-        """
-
-        projector = sklearn.decomposition.KernelPCA(kernel='cosine', random_state=self.SEED,  n_components=n_components)
+        projector = skd.KernelPCA(kernel='cosine', random_state=self.__seed,  n_components=n_components)
         projector.fit(X=matrix)
 
         return projector
+
+    def exc(self, blob: pd.DataFrame,  n_components: int = None, projector: skd.KernelPCA = None) -> (
+            pd.DataFrame, skd.KernelPCA):
+        """
+
+        :param blob: 
+        :param n_components: The number of components of interest
+        :return:
+        """
+
+        # The matrix that will be decomposed
+        matrix: np.ndarray = blob.copy().drop(columns=self.__meta.dependent).array
+
+        # The parallel dependent variable vector, as a series
+        vector: pd.Series = blob.copy()[self.__meta.dependent]
+
+        # The projector
+        if projector is None:
+            projector = self.__projector(matrix=matrix, n_components=n_components)
+
+        # Projecting
+        projected: pd.DataFrame = self.__project(matrix=matrix, vector=vector, projector=projector)
+
+        return projected, projector
