d66cde4f9edf48833090077bd9d9fa655947468d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-08 06:01:20 +0100

    Update src/models/neural.py: In Progress

--- src/models/neural.py
+++ src/models/neural.py
@@ -1,29 +1,29 @@
-import pymc3
+"""A neural network model"""
+import pymc
 import numpy as np
-import theano
-
-import pymc3.theanof
+import pytensor
 
 import config
 
 
 class Neural:
+    """
+    Neural
+        A Bayesian neural network model for beans classification
+    """
 
     def __init__(self):
         """
         Constructor
-
-        Options:
-            * pymc3.theanof.set_tt_rng(pymc3.theanof.MRG_RandomStreams(seed=configurations.SEED))
         """
 
         configurations = config.Config()
-        self.rng = np.random.default_rng(seed=configurations.SEED)
+        self.rng = np.random.default_rng(seed=configurations.seed)
 
     @staticmethod
-    def inference_(model: pymc3.Model, n_iterations: int):
+    def inference_(model: pymc.Model, n_iterations: int):
         """
-        The pymc3.fit( ) **kwargs options are of format -> **{'obj_n_mc': 1000, ...}
+        The pymc.fit( ) **kwargs options are of format -> **{'obj_n_mc': 1000, ...}
 
         :param model:
         :param n_iterations:
@@ -31,12 +31,12 @@ class Neural:
         """
 
         with model:
-            inference = pymc3.FullRankADVI()
-            approximation = pymc3.fit(n=n_iterations, method=inference)
+            inference = pymc.FullRankADVI()
+            approximation = pymc.fit(n=n_iterations, method=inference)
 
             return approximation, inference
 
-    def model_(self, features: np.ndarray, output: np.ndarray) -> pymc3.Model:
+    def model_(self, features: np.ndarray, output: np.ndarray) -> pymc.Model:
         """
 
         :param features:  The feature's tensor, including the bias cells
@@ -45,40 +45,38 @@ class Neural:
         """
 
         # Model
-        with pymc3.Model() as network:
-            """
-            A neural network model consisting of an input layer, two hidden layers, and an output layer
-            """
+        with pymc.Model() as network:
+            # A neural network model consisting of an input layer, two hidden layers, and an output layer
 
             # Architecture
             hidden_1 = 6
             hidden_2 = 5
 
-            init_1 = self.rng.standard_normal(size=(features.shape[1], hidden_1)).astype(theano.config.floatX)
-            init_2 = self.rng.standard_normal(size=(hidden_1, hidden_2)).astype(theano.config.floatX)
-            init_out = self.rng.standard_normal(size=(hidden_2, output.shape[1])).astype(theano.config.floatX)
+            init_1 = self.rng.standard_normal(size=(features.shape[1], hidden_1)).astype(pytensor.config.floatX)
+            init_2 = self.rng.standard_normal(size=(hidden_1, hidden_2)).astype(pytensor.config.floatX)
+            init_out = self.rng.standard_normal(size=(hidden_2, output.shape[1])).astype(pytensor.config.floatX)
 
             # features & Output
-            ann_input = pymc3.Data('ann_input', features)
-            ann_output = pymc3.Data('ann_output', output)
+            ann_input = pymc.Data('ann_input', features)
+            ann_output = pymc.Data('ann_output', output)
 
             # Weights from input to hidden layer
-            weights_in_1 = pymc3.StudentT(name='w_in_1', nu=5, mu=0, sigma=2.5, shape=(features.shape[1], hidden_1),
+            weights_in_1 = pymc.StudentT(name='w_in_1', nu=5, mu=0, sigma=2.5, shape=(features.shape[1], hidden_1),
                                           testval=init_1)
 
             # Weights from 1st to 2nd layer
-            weights_1_2 = pymc3.StudentT(name='w_1_2', nu=5, mu=0, sigma=2.5, shape=(hidden_1, hidden_2), testval=init_2)
+            weights_1_2 = pymc.StudentT(name='w_1_2', nu=5, mu=0, sigma=2.5, shape=(hidden_1, hidden_2), testval=init_2)
 
             # Weights from hidden layer to output
-            weights_2_out = pymc3.Normal(name='w_2_out', mu=0, sigma=1.5, shape=(hidden_2, output.shape[1]),
+            weights_2_out = pymc.Normal(name='w_2_out', mu=0, sigma=1.5, shape=(hidden_2, output.shape[1]),
                                            testval=init_out)
 
             # Build neural-network using tanh activation function
-            act_1 = pymc3.math.tanh(pymc3.math.dot(ann_input, weights_in_1))
-            act_2 = pymc3.math.tanh(pymc3.math.dot(act_1, weights_1_2))
-            act_out = pymc3.math.sigmoid(pymc3.math.dot(act_2, weights_2_out))
+            act_1 = pymc.math.tanh(pymc.math.dot(ann_input, weights_in_1))
+            act_2 = pymc.math.tanh(pymc.math.dot(act_1, weights_1_2))
+            act_out = pymc.math.sigmoid(pymc.math.dot(act_2, weights_2_out))
 
             # Inference
-            pymc3.Bernoulli(name='out', p=act_out, observed=ann_output, total_size=output.shape[0])
+            pymc.Bernoulli(name='out', p=act_out, observed=ann_output, total_size=output.shape[0])
 
         return network
