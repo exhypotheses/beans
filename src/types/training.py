a12a7171f446e234f01a1a573eddf8ec7198d2bd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-10 17:53:46 +0100

    Update src/types/training.py

--- src/types/training.py
+++ src/types/training.py
@@ -3,6 +3,7 @@ Exploring a named tuple class for training data, and complementary objects.
 """
 import typing
 
+import numpy as np
 import pandas as pd
 import sklearn.decomposition as skd
 import sklearn.preprocessing as skp
@@ -21,3 +22,5 @@ class Training(typing.NamedTuple):
     projected: pd.DataFrame = None
     encoded: pd.DataFrame = None
     labels: list = None
+    x_points: np.ndarray = None
+    y_points: np.ndarray = None
