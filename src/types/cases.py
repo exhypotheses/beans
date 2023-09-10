931b42b7155cfc40c3903de03aeefa76601b559d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-10 08:21:28 +0100

    Add src/types/cases.py

---
+++ src/types/cases.py
@@ -0,0 +1,23 @@
+"""
+Exploring named tuple classes
+"""
+import typing
+
+import pandas as pd
+import sklearn.decomposition as skd
+import sklearn.preprocessing as skp
+
+
+class Cases(typing.NamedTuple):
+    """
+    
+    :return: Named tuple class
+    """
+
+    data: pd.DataFrame = None
+    scaler: skp.StandardScaler = None
+    scaled: pd.DataFrame = None
+    projector: skd.KernelPCA = None
+    projected: pd.DataFrame = None
+    encoded: pd.DataFrame = None
+    labels: list = None
