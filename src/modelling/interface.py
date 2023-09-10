4d3c9e9eec95ec49c8dfcebaf990262a787ee86f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-10 20:42:00 +0100

    Update src/modelling/interface.py: In Progress

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -47,8 +47,9 @@ class Interface:
         self.__logger.info('\nY: %s\n%s', training.y_points.shape, training.y_points)
 
         model, details = self.__alg(training=training)
+        
+        # pymc.model.core.Model
         self.__logger.info(model)
-        self.__logger.info(details)
 
-        self.__logger.info(model)
+        # (pymc.variational.approximations.FullRank, pymc.variational.inference.FullRankADVI)
         self.__logger.info(details)
