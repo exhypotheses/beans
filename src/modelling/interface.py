06e1c7045a41f418bcd3d9e7d339ddb38f67dd2d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-11 14:52:33 +0100

    Update src/modelling/interface.py

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -2,11 +2,14 @@
 import logging
 
 import pandas as pd
+import pymc
 
 import src.algorithms.tensors
 import src.modelling.neural
 import src.modelling.preprocessing
 import src.types.training
+import src.inference.sampling.blackjax
+import src.inference.variational.differentiation
 
 
 class Interface:
@@ -33,13 +36,17 @@ class Interface:
         :param train: The training data
         """
 
+        # Preprocessing
         training = src.modelling.preprocessing.Preprocessing().exc(train=train)
         self.__logger.info('Algebraic objects data:\n%s', training.encoded.head())
         self.__logger.info('\nX: %s\n%s', training.x_points.shape, training.x_points)
         self.__logger.info('\nY: %s\n%s', training.y_points.shape, training.y_points)
 
+        # Neural Network Model Architecture
         neural = src.modelling.neural.Neural()
-        model = neural.model_(features=training.x_points, output=training.y_points)
+        model: pymc.Model = neural.model_(features=training.x_points, output=training.y_points)
 
-        # pymc.model.core.Model
-        self.__logger.info(model)
+        # Inference
+        details = src.inference.variational.differentiation.Differentiation().full_rank_advi(
+            model=model, n_iterations=750)
+        self.__logger.info(details)
